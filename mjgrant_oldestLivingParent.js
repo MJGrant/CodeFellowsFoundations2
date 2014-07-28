393921bac38fa5817ea39696068a978c615b47cb
Mandi Grant <mandi018@gmail.com>
2014-07-28 11:00:37 -0700

    Changed to a unique data set, added jquery to display the answer in the browser window.

--- mjgrant_oldestLivingParent.js
+++ mjgrant_oldestLivingParent.js
@@ -1,58 +1,73 @@
+var oldestParentName = "";
+var oldestAge = 0;
+
 /* Sample data set */
-
-
 var people = [
   {
-    name: 'Hank',
-    age: 29,
-    parent: 'Don'
+    name: 'Bran Stark',
+    age: 9,
+    parent: 'Eddard Stark'
   },
   {
-    name: 'Deborah',
-    age: 30,
-    parent: 'Don'
+    name: 'Jon Snow',
+    age: 19,
+    parent: 'Rhaegar Taergaryen'
   },
   {
-    name: 'Danny',
-    age: 24,
-    parent: 'Don'
+    name: 'Robb Stark',
+    age: 16,
+    parent: 'Eddard Stark'
   },
   {
-    name: 'Don',
-    age: 64,
-    parent: 'Haskell'
+    name: 'Rickon Stark',
+    age: 4,
+    parent: 'Eddard Stark'
   },
   {
-    name: 'Kathy',
-    age: 75,
-    parent: 'Lenora'
+    name: 'Eddard Stark',
+    age: 42,
+    parent: 'Rickard Stark'
   },
   {
-    name: 'Haskell',
-    parent: 'Finus'
+    name: 'Jaime Lannister',
+    age:32,
+    parent:'Tywin Lannister'
   },
   {
-    name: 'Finus'
+    name:'Joffrey Baratheon',
+    age:13,
+    parent:'Jaime Lannister'
   },
   {
-    name: 'Donna',
-    age: 43,
-    parent: 'Wilfred'
+    name: 'Tywin Lannister',
+    age:55,
+    parent: 'Tytos Lannister',
   },
   {
-    name: 'Wilfred',
-    age:90,
-    parent: 'George'
+    name: 'Tytos Lannister',
   },
   {
-    name: 'George',
-    age:104,
-    parent: 'The Doctor'
+    name: 'Rhaegar Targaryen',
+    parent: 'Aerys the Mad King'
   },
   {
-    name: 'The Doctor',
-    age:909,
-    parent: 'Gadget',
+    name: 'Aerys the Mad King'
+  },
+  {
+    name: 'Rickard Stark',
+    parent: 'Bran the Builder'
+  },
+  {
+    name: 'Bran the Builder'
+  },
+  {
+    name:'Gendry',
+    age:16,
+    parent:'Robert Baratheon'
+  },
+  {
+    name:'Robert Baratheon',
+    age:41
   },
 ];
 
@@ -67,8 +82,6 @@ function oldestLivingParent(people) {
   });
 
   //Step 2: Iterate through people again, this time grabbing only the ones who are parents and comparing their age to the last age saved
-  var oldestParentName = "";
-  var oldestAge = 0;
   people.forEach(function(person) {
     if (parents[person.name] && person.age > oldestAge) { //if this person is found in the parents set AND the age is higher than the oldest age
       oldestAge = person.age;
@@ -77,9 +90,11 @@ function oldestLivingParent(people) {
   });
 
   //Step 3: Return the name of the person with the highest age
-  console.log("Oldest living parent: ", oldestParentName + " who is " + oldestAge);
   return oldestParentName;
 }
 
 oldestLivingParent(people);
 
+$(document).ready(function () {
+  $('body').append("Oldest living parent: ", oldestParentName + " who is " + oldestAge);
+});
